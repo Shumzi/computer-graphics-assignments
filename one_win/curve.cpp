a680d5a196cb8465ef89e62fc48c0928999885d2
Ariel Shumacher <ariegg210@gmail.com>
2025-12-20 16:54:03 +0200

    found bsp-to-bez mat and implemented evalBsp using it.

--- one_win/curve.cpp
+++ one_win/curve.cpp
@@ -22,7 +22,7 @@ namespace
 Vector3f qBez(float t, Vector3f p_0, Vector3f  p_1, Vector3f  p_2, Vector3f  p_3)
 {     
     // remembering that
-    // q(t) = (1 - t) ^ 3P_0 + 3(1 - t) ^ 2 * tP_1 + 3(1 - t)t ^ 2 * P_2 + t ^ 3 * P_3
+    // q(t) = (1 - t) ^ 3 * P_0 + 3(1 - t) ^ 2 * tP_1 + 3(1 - t)t ^ 2 * P_2 + t ^ 3 * P_3
     Vector3f p_0_comp = pow(1 - t, 3) * p_0;
     Vector3f p_1_comp = 3 * pow(1 - t, 2) * t * p_1;
     Vector3f p_2_comp = 3 * (1 - t) * pow(t, 2) * p_2;
@@ -128,7 +128,7 @@ Curve evalBezier( const vector< Vector3f >& P, unsigned steps )
 
 	Curve result;
 
-    /* we need to calc n,b,t,v i.e. the normal, binormal, tangent and vertex location of <steps> points on the 
+    /* we need to calc n,b,t,v i.e. the normal, binormal, tangent and vertex location of <steps> points on the curve
     * defined by the control points P of a piecewise polynomial Bezier curve.
     * steps:
     * 1. assume P is on the xy plane (i.e. z is always 0) to get a feel of how to implement this.
@@ -164,6 +164,55 @@ Curve evalBezier( const vector< Vector3f >& P, unsigned steps )
     return result;
 }
 
+vector<Vector3f> bspToBez(Vector3f p_0, Vector3f p_1, Vector3f p_2, Vector3f p_3)
+{
+    /*
+    * remembering that we use the following to convert between bases:
+    * (G*B_0*B_1^-1)*B_1*T
+    * we'll use the mat we found in findBspToBezMat for the B_0*B_1^-1.
+    * now all that's left is to multiply it by our cps G to get their bez equivalents.
+    */
+    vector<Vector3f> res;
+    Matrix4f bsp_to_bez_mat = Matrix4f(1, 0, 0, 0, 4, 4, 2, 1, 1, 2, 4, 4, 0, 0, 0, 1);
+    bsp_to_bez_mat /= 6;
+    // sadly no matrix3x4 exists so we'll use what we've got...
+    Matrix4f cps = Matrix4f(Vector4f(p_0, 0), Vector4f(p_1, 0), Vector4f(p_2, 0), Vector4f(p_3, 0));
+    Matrix4f bez_cps = cps * bsp_to_bez_mat;
+    // now we'll extract the relevant vectors, disregarding the last row of 0's
+    res.push_back(bez_cps.getCol(0).xyz());
+    res.push_back(bez_cps.getCol(1).xyz());
+    res.push_back(bez_cps.getCol(2).xyz());
+    res.push_back(bez_cps.getCol(3).xyz());
+    return res;
+}
+
+void findBspToBezMat()
+{
+    Matrix4f bez(1, -3, 3, -1, 0, 3, -6, 3, 0, 0, 3, -3, 0, 0, 0, 1);
+    cerr << "bez mat:" << endl;
+    bez.print();
+    Matrix4f bsp(1, -3, 3, -1, 4, 0, -6, 3, 1, 3, 3, -3, 0, 0, 0, 1);
+    bsp /= 6;
+    cerr << "bsp mat:" << endl;
+    bsp.print();
+    Matrix4f inv_bez = bez.inverse();
+    cerr << "bsp^-1 mat:" << endl;
+    inv_bez.print();
+    Matrix4f conversion_mat = bsp * inv_bez;
+    cerr << "conversion mat:" << endl;
+    conversion_mat.print();
+    /* result:
+    * [ 0.1667 0.0000 0.0000 0.0000 ]
+    * [ 0.6667 0.6667 0.3333 0.1667 ]
+    * [ 0.1667 0.3333 0.6667 0.6667 ]
+    * [ 0.0000 0.0000 0.0000 0.1667 ]
+    * bez-bsp (accidentally did the wrong way at first):
+    * [ 6.0000 -0.0000 0.0000 0.0000 ]
+    * [ -7.0000 2.0000 -1.0000 2.0000 ]
+    * [ 2.0000 -1.0000 2.0000 -7.0000 ]
+    * [ 0.0000 0.0000 0.0000 6.0000 ]
+    */
+}
 
 Curve evalBspline( const vector< Vector3f >& P, unsigned steps )
 {
@@ -178,7 +227,8 @@ Curve evalBspline( const vector< Vector3f >& P, unsigned steps )
     // It is suggested that you implement this function by changing
     // basis from B-spline to Bezier.  That way, you can just call
     // your evalBezier function.
-
+//    cerr << "\t>>> just printing the transition mat for once:" << endl;
+//    findBspToBezMat();
     cerr << "\t>>> evalBSpline has been called with the following input:" << endl;
 
     cerr << "\t>>> Control points (type vector< Vector3f >): "<< endl;
@@ -186,12 +236,21 @@ Curve evalBspline( const vector< Vector3f >& P, unsigned steps )
     {
         cerr << "\t>>> " << P[i] << endl;
     }
+    Curve result;
+    vector<Vector3f> bez_cps;
+    // converting to bez
+    for (unsigned i = 0; i < P.size() - 3; ++i)
+    {
+        vector<Vector3f> equivalent_bez_cps = bspToBez(P[i], P[i + 1], P[i + 2], P[i + 3]);
+        bez_cps.push_back(equivalent_bez_cps[0]);
+        bez_cps.push_back(equivalent_bez_cps[1]);
+        bez_cps.push_back(equivalent_bez_cps[2]);
+        bez_cps.push_back(equivalent_bez_cps[3]);
+    }
+    result = evalBezier(bez_cps, steps);
 
     cerr << "\t>>> Steps (type steps): " << steps << endl;
-    cerr << "\t>>> Returning empty curve." << endl;
-
-    // Return an empty curve right now.
-    return Curve();
+    return result;
 }
 
 Curve evalCircle( float radius, unsigned steps )

8e293dfd77244e5de8f93964a0bf0e29993eaed6
Ariel Shumacher <ariegg210@gmail.com>
2026-03-17 21:29:42 +0200

    two-shot the transform, just had to make sure normal vector was normalized after tfm.
    good thing my intersection funcs didn't assume ray direction is unit length.

--- A4/Transform.h
+++ A4/Transform.h
@@ -10,17 +10,35 @@ class Transform: public Object3D
 {
 public: 
   Transform(){}
- Transform( const Matrix4f& m, Object3D* obj ):o(obj){
-    
+  /**
+   * for points - M^(-1)*p
+   * for normals - M^(-T).
+   */
+  Transform( const Matrix4f& m, Object3D* obj ):o(obj) 
+  {
+    rayMat = m.inverse();
+    normalMat = rayMat.transposed();
   }
   ~Transform(){
   }
   virtual bool intersect( const Ray& r , Hit& h , float tmin){
-    o->intersect( r , h , tmin);
+    Vector3f newOrigin = (rayMat * Vector4f(r.getOrigin(),1)).xyz();
+    Vector3f newDirection = (rayMat * Vector4f(r.getDirection(), 0)).xyz();    
+    Ray newR(newOrigin, newDirection);
+    // note that normal at point will be unaltered completely bc we only changed the ray, not the geometry.
+    bool intersected = o->intersect( newR , h , tmin); 
+    if(intersected)
+    {
+      // fix normals.
+      Vector3f newNormal = (normalMat * Vector4f(h.getNormal(), 0)).xyz().normalized();
+      h.set(h.getT(), h.getMaterial(), newNormal);
+    }
   }
 
  protected:
   Object3D* o; //un-transformed object	
+  Matrix4f rayMat;
+  Matrix4f normalMat;
 };
 
 #endif //TRANSFORM_H
